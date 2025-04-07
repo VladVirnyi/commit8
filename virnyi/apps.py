7a6937c5ddcc2b6a1605b375b73ea824e659be96
Vlad Virnyi <vladvwv15@gmail.com>
2025-04-07 17:10:51 +0300

    lab4

---
+++ virnyi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VirnyiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'virnyi'
---
+++ virnyi/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from virnyi import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('page1/', views.page1, name='page1'),
+    path('page2/', views.page2, name='page2'),
+    path('page3/', views.page3, name='page3'),
+    path('page4/', views.page4, name='page4'),
+]
