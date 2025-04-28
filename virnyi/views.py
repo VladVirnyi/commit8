e99807ec1b0a8d457036ded0217fb4a4c5d511f5
Vlad Virnyi <vladvwv15@gmail.com>
2025-04-28 16:00:20 +0300

    lab7

--- virnyi/views.py
+++ virnyi/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import MenuCategory, MenuItem
 
 
 def home(request):
@@ -19,7 +20,29 @@ def page3(request):
 
 def page4(request):
     return render(request, 'page4.html')
-from django.shortcuts import render
 
-from django.shortcuts import render
+
+def category_list(request):
+    categories = MenuCategory.objects.filter(is_available=True)
+    return render(request, 'category_list.html', {
+        'categories': categories
+    })
+
+
+def category_detail(request, category_id):
+    category = get_object_or_404(MenuCategory, id=category_id)
+    products = MenuItem.objects.filter(category=category, is_available=True)
+    return render(request, 'category_detail.html', {
+        'category': category,
+        'products': products
+    })
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(MenuItem, id=product_id)
+    return render(request, 'product_detail.html', {
+        'product': product
+    })
+
+
 
